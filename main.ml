6a2fd69f82427f2174e3e4fdef4408db3562496d
Vincent <schoonheere.vincent.pro@outlook.fr>
2026-04-26 21:10:27 +0200

    clean code, no more var d'env pour le mode interactif

--- main.ml
+++ main.ml
@@ -4,43 +4,35 @@ open Graphics
 exception Division_by_zero
 
 let is_tested = Sys.getenv_opt "NO_WAIT" = None
-let is_interactif = Sys.getenv_opt "MODE_INTER" <> None
+let is_interactif = Unix.isatty Unix.stdin
 
+let parse lexbuf = try Parser.programme Lexer.token lexbuf
+                with 
+                | Lexer.Error a -> Printf.eprintf "Erreur lexicale %s\n" a; exit 1
+                | Parser.Error -> let pos = lexbuf.Lexing.lex_curr_p in Printf.eprintf "Erreur syntaxique à la ligne : %d et à la colonne %d\n" pos.pos_lnum (pos.Lexing.pos_cnum - pos.Lexing.pos_bol ); exit 1
 
-let () =
-        init_graphics () ;
-        
-        if is_interactif 
-        then 
-                begin 
+let mode_interactif () = 
                 Printf.printf "mode interactif :\n> ";
                 let envext = ref [] in 
                 let buf = Buffer.create 512 in
                 while true do 
                         Buffer.clear buf;
-                        try 
-                                while true do
-                                        Buffer.add_string buf (read_line());
+                        (try while true do
+                                Buffer.add_string buf (read_line());
                                 done 
                         with 
-                        | End_of_file -> Printf.printf "> "; let lexbuf = Lexing.from_string (Buffer.contents buf) in
-                                         let ast =  try Parser.programme Lexer.token lexbuf
-                                                with 
-                                                | Lexer.Error a -> Printf.eprintf "Erreur lexicale %s\n" a; exit 1
-                                                | Parser.Error -> let pos = lexbuf.Lexing.lex_curr_p in Printf.eprintf "Erreur syntaxique à la ligne : %d et à la colonne %d\n" pos.pos_lnum (pos.Lexing.pos_cnum - pos.Lexing.pos_bol ); exit 1
-                                        in envext := decode ~initial_decla:!envext ast 
+                        | End_of_file -> ());
+                        Printf.printf "> "; 
+                        let ast = parse (Lexing.from_string (Buffer.contents buf)) in envext := decode ~initial_decla:!envext ast;
                 done
-                end 
-        else 
-             begin    
-                let lexbuf = Lexing.from_channel stdin in 
+                
+let mode_fichier () = let ast =  parse (Lexing.from_channel stdin) in
+                        ignore(decode ast);
+                        if is_tested then ignore(read_key())
 
-                let ast =  try Parser.programme Lexer.token lexbuf
-                        with 
-                        | Lexer.Error a -> Printf.eprintf "Erreur lexicale %s\n" a; exit 1
-                        | Parser.Error -> let pos = lexbuf.Lexing.lex_curr_p in Printf.eprintf "Erreur syntaxique à la ligne : %d et à la colonne %d\n" pos.pos_lnum (pos.Lexing.pos_cnum - pos.Lexing.pos_bol ); exit 1
-
-                 in ignore(decode ast) ; 
-                if is_tested then ignore(read_key()) ;
-                end ;
-        close_graph () 
+let () =
+        init_graphics () ;
+        (if is_interactif 
+        then mode_interactif ()
+        else mode_fichier ());
+        close_graph ()
