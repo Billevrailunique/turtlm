a38862a80300ce05a992a80c846b753a76274d9d
Vincent <schoonheere.vincent.pro@outlook.fr>
2026-06-17 21:11:48 +0200

    enhance error message using menhirlib

--- main.ml
+++ main.ml
@@ -8,6 +8,7 @@ let is_interactif = Unix.isatty Unix.stdin
 
 module Err = MenhirLib.ErrorReports
 module MInter = Parser.MenhirInterpreter
+module Lex = MenhirLib.LexerUtil
 
 (*n'est executé qu'une fois, lorsqu'on réduit à l'axiome*)
 let run v = try ignore(decode v)
@@ -31,35 +32,42 @@ with
         | _ -> eprintf "erreur non pris en charge" 
         
 
-let env checkpoint =
+let get_env checkpoint =
   match checkpoint with
   | MInter.HandlingError env -> env
   | _ -> assert false
 
 let state checkpoint =
-  MInter.current_state_number (env checkpoint)
+  MInter.current_state_number (get_env checkpoint)
 
-let syntax_error checkpoint = let num = (state checkpoint) in printf "%s(echec état %d)\n" (ParserMessages.message num) num ;  close_graph () 
+let syntax_error checkpoint buffer source = 
+  let num = (state checkpoint) in
+  let location = Lex.range (Err.last buffer) in
+  let indication = sprintf  "Erreur syntaxique (echec in state %d) %s\n" num (Err.show (Err.extract source) buffer) in 
+  eprintf "%s%s%s" location indication (ParserMessages.message num);  close_graph () 
 
-let rec parse lexbuf checkpoint =  
+let rec parse lexbuf buffer supplier source checkpoint =  
   match checkpoint with
-    | MInter.InputNeeded _ -> (try (let token = Lexer.token lexbuf in 
-                                let startp = lexbuf.lex_start_p
-                                and endp = lexbuf.lex_curr_p in
-                                let checkpoint = MInter.offer checkpoint (token, startp, endp) in 
-                                parse lexbuf checkpoint)
+    | MInter.InputNeeded _ -> (try 
+                                let checkpoint = MInter.offer checkpoint (supplier ()) in 
+                                parse lexbuf buffer supplier source checkpoint
                               with
                               | Lexer.Error msg -> printf "Erreur lexicale %s\n" msg; exit 1)
     | MInter.Shifting _ 
-    | MInter.AboutToReduce _ -> let checkpoint = MInter.resume checkpoint in parse lexbuf checkpoint 
-    | HandlingError _ -> syntax_error checkpoint
+    | MInter.AboutToReduce _ -> let checkpoint = MInter.resume checkpoint in parse lexbuf buffer supplier source checkpoint 
+    | HandlingError _ -> syntax_error checkpoint buffer source
     | Accepted v -> run v 
     | Rejected -> assert false
 
-
-let mode_fichier () = let lexbuf = Lexing.from_channel stdin in parse lexbuf (Parser.Incremental.programme lexbuf.lex_curr_p);
-                      if is_tested then try ignore(read_key ()) 
-                                        with | _ -> ()
+let mode_fichier () = 
+  let source,lexbuf = Lex.read (Sys.argv.(1)) in 
+  let supplier = MInter.lexer_lexbuf_to_supplier Lexer.token lexbuf in 
+  let buffer, supplier = Err.wrap_supplier supplier in 
+  parse lexbuf buffer supplier source (Parser.Incremental.programme lexbuf.lex_curr_p); 
+  if is_tested 
+  then 
+    try ignore(read_key ()) 
+    with | _ -> ()
 
 let () = init_graphics ();
         mode_fichier () ;
