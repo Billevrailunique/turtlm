eabaee7556d33606c9bf46a2ada878dcb9616031
Vincent <schoonheere.vincent.pro@outlook.fr>
2026-06-17 22:22:21 +0200

    enhance running error message and re-add pretraitement

--- main.ml
+++ main.ml
@@ -10,27 +10,38 @@ module Err = MenhirLib.ErrorReports
 module MInter = Parser.MenhirInterpreter
 module Lex = MenhirLib.LexerUtil
 
+
+let running_error e = 
+  let msg,pos,while_running = match e with
+    | Division_by_zero pos ->  "division par 0\n", Some pos,true
+    | TooManyArgsException (name,pos) -> sprintf "trop d'argument donné à la fonction %s\n" name,Some pos, false
+    | ArgsMissingException (name ,pos) -> sprintf "pas assez d'argument donné à la fonction %s\n" name, Some pos, false
+    | EnvEmpty ->  "environnement vide lorsque dépiler", None,true
+    | OutOfBoundsCursor pos ->  "curseur en dehors de l'écran\n", Some pos, true
+    | OutOfBoundsPencilWidth pos ->  "largeur pinceau trop grande\n", Some pos , true
+    | AlreadyDeclaredVar (name,pos) -> sprintf "variable %s déjà déclaré\n" name, Some pos, false
+    | AlreadyDeclaredFun  (name,pos) -> sprintf "fonction %s déjà déclaré\n" name, Some pos, false
+    | UnknownFun (name,pos) -> sprintf  "fonction %s inconnu\n" name, Some pos, false
+    | UnknownVar (name, pos) -> sprintf "variable %s inconnu\n" name,Some pos, false
+    | OutOfContextReturn pos ->  "return en dehors d'une fonction\n", Some pos, false
+    | NotYetInitVar (name,pos) -> sprintf "variable %s pas encore initialisé\n" name, Some pos, false
+    | Invalid_argumentGenN pos ->  "GenN mal utiliser\n", Some pos, true
+    | FloatWaited pos->  "float attandu\n", Some pos, true
+    | BoolWaited pos->  "bool attendu\n", Some pos, true
+    | ColorWaited pos->  "couleur attendu\n", Some pos, true
+    | _ -> eprintf "erreur non pris en charge"; exit 1 
+  in
+  let location = sprintf "File \"%s\"\n" Sys.argv.(1) in
+  let type_err = sprintf "Error %s" (if while_running then "Run : " else "PreRun : ")  in 
+  let indication = match pos with 
+                    | Some e -> sprintf "à la ligne %d, char %d\n" e.Lexing.pos_lnum e.Lexing.pos_cnum 
+                    | None -> "\n" in
+  eprintf "%s%s%s%s" location type_err indication msg; exit 1
+
+
 (*n'est executé qu'une fois, lorsqu'on réduit à l'axiome*)
-let run v = try ignore(decode v)
-with 
-        | Division_by_zero pos -> eprintf "division par 0 à la ligne %d\n" pos.Lexing.pos_lnum
-        | TooManyArgsException (name,pos) -> eprintf "trop d'argument donné à la fonction %s à la ligne %d\n" name pos.Lexing.pos_lnum
-        | ArgsMissingException (name ,pos) -> eprintf "pas assez d'argument donné à la fonction %s à la ligne %d\n" name pos.Lexing.pos_lnum
-        | EnvEmpty -> eprintf "environnement vide lorsque dépiler"
-        | OutOfBoundsCursor pos -> eprintf "curseur en dehors de l'écran à la ligne %d\n" pos.Lexing.pos_lnum
-        | OutOfBoundsPencilWidth pos -> eprintf "largeur pinceau trop grande à la ligne %d\n" pos.Lexing.pos_lnum
-        | AlreadyDeclaredVar (name,pos) -> eprintf "variable %s déjà déclaré quand on arrive à la ligne %d\n" name pos.Lexing.pos_lnum
-        | AlreadyDeclaredFun  (name,pos) -> eprintf "fonction %s déjà déclaré quand on arrive à la ligne %d\n" name pos.Lexing.pos_lnum
-        | UnknownFun (name,pos) -> eprintf "fonction %s inconnu à la ligne %d\n" name pos.Lexing.pos_lnum
-        | UnknownVar (name, pos) -> eprintf "variable %s inconnu à la ligne %d\n" name pos.Lexing.pos_lnum
-        | OutOfContextReturn pos -> eprintf "return en dehors d'une donction à la ligne %d\n" pos.Lexing.pos_lnum
-        | NotYetInitVar (name,pos) -> eprintf "variable %s pas encore initialisé à la ligne %d\n" name pos.Lexing.pos_lnum
-        | Invalid_argumentGenN pos -> eprintf "GenN mal utiliser à la ligne %d\n" pos.Lexing.pos_lnum
-        | FloatWaited pos-> eprintf "float attandu à la ligne %d\n" pos.Lexing.pos_lnum
-        | BoolWaited pos-> eprintf "bool attendu à la ligne %d\n" pos.Lexing.pos_lnum
-        | ColorWaited pos-> eprintf "couleur attendu à la ligne %d\n" pos.Lexing.pos_lnum
-        | _ -> eprintf "erreur non pris en charge" 
-        
+let run v = try ignore(pretraitement v);ignore(decode v)
+with e -> running_error e
 
 let get_env checkpoint =
   match checkpoint with
@@ -53,8 +64,8 @@ let rec parse lexbuf buffer supplier source checkpoint =
                                 parse lexbuf buffer supplier source checkpoint
                               with
                               | Lexer.Error msg -> printf "Erreur lexicale %s\n" msg; exit 1)
-    | MInter.Shifting _ 
-    | MInter.AboutToReduce _ -> let checkpoint = MInter.resume checkpoint in parse lexbuf buffer supplier source checkpoint 
+    | Shifting _ 
+    | AboutToReduce _ -> let checkpoint = MInter.resume checkpoint in parse lexbuf buffer supplier source checkpoint 
     | HandlingError _ -> syntax_error checkpoint buffer source
     | Accepted v -> run v 
     | Rejected -> assert false
